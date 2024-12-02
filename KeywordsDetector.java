6b1d4d433df60628351e9915b4a9860e9fcd0181
Dan Peled <mywebdan@gmail.com>
2024-12-02 15:59:42 +0200

    KeywordsDetector - V1

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -23,15 +23,15 @@ public class KeywordsDetector {
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         
         for (int i = 0; i < sentences.length; i++){
-            String sentenceToCheck = sentences[i];
-            System.out.println("Sentence: " + sentenceToCheck);
+            String sentenceToCheck = lowerCase(sentences[i]); 
+            //System.out.println("Sentence: " + sentenceToCheck);
 
             for (int j = 0; j < keywords.length; j++){
-                String keywordToCheck = keywords[j];
-                System.out.println("Keyword: " + keywordToCheck);
+                String keywordToCheck = lowerCase(keywords[j]);
+                //System.out.println("Keyword: " + keywordToCheck);
                 
                 if (contains(sentenceToCheck, keywordToCheck)){
-                    System.out.println("contain");
+                    System.out.println(sentenceToCheck);
                     break;
                 }
 
@@ -39,6 +39,26 @@ public class KeywordsDetector {
         }
         
 
+    }
+
+
+    public static String lowerCase(String str) {
+        // Replace the following statement with your code
+        String lowercaseString = "";
+        char letterLower;
+
+        for (int i = 0 ; i < str.length(); i++){
+            int letter_ascii = str.charAt(i);
+            if (65 <= letter_ascii && letter_ascii <= 90){
+                int lowerletter_ascii = letter_ascii + 32;
+                letterLower = (char)lowerletter_ascii;
+            }else{
+                letterLower = str.charAt(i);
+            }
+
+            lowercaseString += letterLower;
+        }
+        return lowercaseString;
     }
 
     public static boolean contains(String str1, String str2) {
@@ -49,22 +69,28 @@ public class KeywordsDetector {
             return true;
         }
 
+        if (str2.length() > str1.length()){
+            return false;
+        }
+
         char charToCheck = str2.charAt(0);
         for (int j = 0; j < str1.length()-1; j++){
             if (charToCheck == str1.charAt(j)) {
-                //System.out.println("Found first letter");
+               // System.out.println("Found first letter ");
                 for (int i = 0; i < str2.length(); i++ ){
                     if (str2.charAt(i) != str1.charAt(j)) {
                         //System.out.println("Found not equal "+ str2.charAt(i) +" " + str1.charAt(j));
                         break;
                     }
                     j++;
+                    if (j == str1.length()) {
+                        break;
+                    }
                     if (i == str2.length()-1){
                         return true;
                     }
-                }
-               
-            }
+                }               
+            }  
         }
         return false;
     }
